d97de3d524a73a3f0f28eed286f584260e0d08d7
Frankie <zhuangfrankie@gmail.com>
2021-03-21 22:58:44 -0700

    refractored server code

--- kbfinder/pages/api/hello.js
+++ kbfinder/pages/api/hello.js
@@ -1,5 +1,32 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const http = require("http");
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+const req = http.request(
+  {
+    host: "127.0.0.1",
+    port: 3000,
+    method: "GET",
+  },
+  (res) => {
+    console.log("tyoyoyoyoyoyo");
+    res.resume();
+    res.on("end", () => {
+      if (!res.complete)
+        console.error(
+          "The connection was terminated while the message was still being sent"
+        );
+    });
+  }
+);
+
+// export default (req, res) => {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+// export default function handler(req, res) {
+//   if (req.method === "GET") {
+//     console.log("WORKEDDDDDDDDDDDDDDDDDDDDDDDD")
+//     res.status(200).json({"GOT SOMETHING BACK"})
+//   } else {
+//     res.status(400).json("NOPEEEEEEEEEEEEEEEEEEEEEEEEEE")
+//   }
+// }
--- kbfinder/pages/index.js
+++ kbfinder/pages/index.js
@@ -21,49 +21,46 @@ export default function Home() {
           <a href="https://github.com/justfrankie/kbfinder">KBfinder</a>
         </h1>
 
-        <p className={styles.description}>
+        <div className={styles.description}>
           <p className={styles.code}>
-            A tool used to find average pricing for keycaps from r/mechmarket
+            A tool used to find average pricing for keycaps from /r/mechmarket
           </p>
-          <div class="input-group rounded">
+          <div className="input-group rounded">
             <input
               type="search"
-              class="form-control rounded inputBar"
+              className="form-control rounded inputBar"
               placeholder="Search"
               aria-label="Search"
               aria-describedby="search-addon"
               id={styles.inputBar}
             />
-            <span class="input-group-text border-0" id="search-addon">
-              <i class="fas fa-search"></i>
+            <span className="input-group-text border-0" id="search-addon">
+              <i className="fas fa-search"></i>
             </span>
           </div>
-        </p>
+        </div>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Header 1 &rarr;</h3>
+          <a href="www.reddit.com/r/mechmarket/" className={styles.card}>
+            <h3>r/mechmarket &rarr;</h3>
             <p>Text Placeholder 1</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Header 2 &rarr;</h3>
+          <a href="www.mechgroupbuys.com/" className={styles.card}>
+            <h3>mechgroupbuys.com &rarr;</h3>
             <p>Text Placeholder 2</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href="https://keycaplendar.firebaseapp.com/?page=calendar"
             className={styles.card}
           >
-            <h3>Header 3 &rarr;</h3>
+            <h3>keycaplendar.com &rarr;</h3>
             <p>Text Placeholder 3</p>
           </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Header 4 &rarr;</h3>
+          <a href="https://keycapsets.com/" className={styles.card}>
+            <h3>keycapsets.com &rarr;</h3>
             <p>Text Placeholder 4</p>
           </a>
         </div>
@@ -83,17 +80,17 @@ export default function Home() {
       <script
         src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       ></script>
       <script
         src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
         integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       ></script>
       <script
         src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       ></script>
     </div>
   );
