8712eabb8cfeb52f496780dbb677a7f77b55a6f4
Frankie <zhuangfrankie@gmail.com>
2021-04-19 23:59:41 -0700

    remove buffer line

--- kbfinder/pages/api/hello.js
+++ kbfinder/pages/api/hello.js
@@ -4,7 +4,7 @@
 // const cors = require("cors");
 // const path = require("path");
 
-global.Buffer = global.Buffer || require("buffer").Buffer;
+// global.Buffer = global.Buffer || require("buffer").Buffer;
 
 if (typeof btoa === "undefined") {
   global.btoa = function (str) {
